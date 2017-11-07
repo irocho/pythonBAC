66e7994b20ef7995b00baed5ee49e6a640fbc2e9
i-rochiño <irocho.profe@gmail.com>
2017-11-07 20:11:33 +0100

    Datos que deben ir no código a entregar en clase

---
+++ principio.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#############################################
+# Este código permite ......
+#
+# Nome de ficheiro: 
+# Autor:
+# URL:
+# Repositorio:
+# TODO:
+#
+###############################################
