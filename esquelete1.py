b88c783c3654f70745d543f5021cb0d711e5249c
i-rochiño <irocho.profe@gmail.com>
2017-11-07 20:22:52 +0100

    Exercicio sinxelo do uso de input()

---
+++ esquelete1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+##############################################################################
+# Este código pregunta ó usuario daots persoais e mostra a ficha obtida
+#
+# Nome de ficheiro: 
+# Autor:
+# URL:
+# Repositorio:
+# TODO:
+#
+##############################################################################
+
+# Pregunta ó usuario o seu nome 
+
+# Mostra en pantalla un saúdo persoalizado 
+
+# Pregunta ó usuario o seu DNI
+
+# Pregunta a idade
+
+# Pregunta a localidade
+
+# Mostra en pantalla un os datos obtidos
