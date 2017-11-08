2adf6de4d9f460ec39dd4448f9c85e47af140c1a
i-rochiño <irocho.profe@gmail.com>
2017-11-08 17:04:01 +0100

    Esquelete para simular a caixa rápida do súper

---
+++ 2_caixaSuper.py
@@ -0,0 +1,28 @@
+
+
+
+# Engade arriba a primeira liña de todos os .py
+
+######################################################
+# Este código permite simular a caixa rápida do súper
+#
+# Nome de ficheiro: 
+# Autor:
+# URL:
+# TODO:
+#
+######################################################
+
+# Saúda educadamente ó usuario
+
+
+# Pide  o prezo do primeiro producto
+
+
+# Pide  o prezo do segundo producto
+
+
+# Pide o prezo do terceiro producto
+
+
+# Mostra na pantalla a suma dos tres prezos
