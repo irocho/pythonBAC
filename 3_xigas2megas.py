75208f9b362fa2a1dfc65874b3c5a84f01f3d5cb
i-rochiño <irocho.profe@gmail.com>
2017-11-08 17:26:38 +0100

    Convirte os xigas que dá  o usuario en megas

---
+++ 3_xigas2megas.py
@@ -0,0 +1,20 @@
+
+# Engade arriba a primeira liña de todos os .py
+
+######################################################
+# Este código convirte os XB que dá  o usuario en MB
+#
+# Nome de ficheiro: 
+# Autor:
+# URL:
+# TODO:
+#
+######################################################
+
+# Mostra en pantalla unha indicación do que tenque facer o usuario
+
+# Pregunta ó usuario cantos xigas ten
+
+# Convirte os xigas a megas
+
+# Mostra ó usuario a resposta
